9d9e436a33bf4eab8f508a81f990a0f92c50c8e6
christopher <christopher@datamaran.com>
2025-12-26 23:48:02 +0100

    move stories into .storybook

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,6 +1,6 @@
 /** @type { import('@storybook/vue3-vite').StorybookConfig } */
 const config = {
-  stories: ["../storybook/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  stories: ["./stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
   addons: ["@storybook/addon-essentials", "@storybook/addon-interactions"],
   framework: "@storybook/vue3-vite",
 };
