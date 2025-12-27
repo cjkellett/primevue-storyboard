03f8e1f76d8d42036491f695af30e1ced16ac3fc
christopher <christopher@datamaran.com>
2025-12-27 17:51:45 +0100

    tabs example

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,41 @@
 <script setup>
-import Button from "primevue/button";
+import Tabs from "primevue/tabs";
+import TabList from "primevue/tablist";
+import Tab from "primevue/tab";
+import TabPanels from "primevue/tabpanels";
+import TabPanel from "primevue/tabpanel";
 </script>
 
 <template>
   <div class="container">
-    <h1>Hello World</h1>
-    <Button v-tooltip.top="'Hello World'" label="Hello World" />
+    <Tabs value="0">
+      <TabList>
+        <Tab value="0">Home</Tab>
+        <Tab value="1">About</Tab>
+      </TabList>
+      <TabPanels>
+        <TabPanel value="0">
+          <h1>Home</h1>
+          <p>Welcome to the Home page!</p>
+        </TabPanel>
+        <TabPanel value="1">
+          <h1>About</h1>
+          <p>This is the About page.</p>
+        </TabPanel>
+      </TabPanels>
+    </Tabs>
   </div>
 </template>
 
 <style scoped>
 .container {
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
+  padding: 2rem;
+  max-width: 1200px;
+  margin: 0 auto;
   min-height: 100vh;
-  gap: 2rem;
 }
 
 h1 {
-  margin: 0;
+  margin: 0 0 1rem 0;
 }
 </style>
