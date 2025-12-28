4d76cb54236e7b747d1652b3a55d9f99f1478640
christopher <christopher@datamaran.com>
2025-12-28 10:02:41 +0100

    simplify

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
-import { tailwindThemePlugin } from "./vite-plugins/tailwind-theme.js";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), tailwindThemePlugin()],
+  plugins: [vue()],
 });
